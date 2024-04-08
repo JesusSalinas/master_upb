64df18137da91639cb877434dfab03259eb9ea48
Jesus Salinas <jsalinas@Jesuss-MacBook-Pro.local>
2024-04-07 19:44:39 -0500

    MongoDBConnectorClass

--- src/mongo.py
+++ src/mongo.py
@@ -1,39 +1,46 @@
 
+import os
+
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from urllib.parse import quote_plus
-
-import os
 from dotenv import load_dotenv
 load_dotenv()
 
-password = quote_plus(os.getenv('MONGO_PASS'))
-user = quote_plus(os.getenv('MONGO_USER'))
-db = os.getenv('MONGO_DB')
-collection = os.getenv('MONGO_COLLECTION')
+env_password = quote_plus(os.getenv('MONGO_PASS'))
+env_user = quote_plus(os.getenv('MONGO_USER'))
+env_db = os.getenv('MONGO_DB')
+env_host = os.getenv('MONGO_HOST')
+env_uri = 'mongodb+srv://' + env_user+ ':' + env_password + '@' + env_host + '/'
 
-uri = 'mongodb+srv://' + user+ ':' + password + '@jesuscluster.hl2zshw.mongodb.net/?retryWrites=true&w=majority&appName=JesusCluster'
-
-# Create a new client and connect to the server
-client = MongoClient(uri, server_api=ServerApi('1'))
-
-db = client[db]
-
-co = db[collection]
-
-# Send a ping to confirm a successful connection
-try:
-    client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
-except Exception as e:
-    print(e)
-
-documentos = co.find()
-
-print("Documentos en la colección:")
-for documento in documentos:
-    print(documento)
-
-
-# Cerrar la conexión
-client.close()
+class MongoDBConnector:
+    def __init__(self):
+        self.host = env_host
+        self.username = env_user
+        self.password = env_password
+        self.database_name = env_db
+        self.db = None
+        self.client = None
+        self.collection = None
+    
+    def connect(self):
+        try:
+            self.client = MongoClient(env_uri, server_api=ServerApi('1'))
+            self.db = self.client[self.database_name]
+            print('DB CONNECTION SUCCESSFULL!:', self.database_name)
+        except Exception as e:
+            print("DB CONNECTION FAILED!:", e)
+    
+    def disconnect(self):
+        try:
+            self.client.close()
+            print('DB DISCONNECTION SUCCESSFULL!')
+        except Exception as e:
+            print('DB DISCONNECTION FAILED!:', e)
+    
+    def get_collection(self, collection_name):
+        try:
+            self.collection = self.db[collection_name]
+            print('COLLECTION FETCH SUCCESSFULL!')
+        except Exception as e:
+            print('COLLECTION FETCH FAILED!', e)
